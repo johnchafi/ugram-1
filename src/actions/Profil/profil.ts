726712eb95fb8a0ca0910a2c70a263e7a1ce9743
Maxime Leroy <maxime.leroy.4@ulaval.ca>
2019-02-11 14:04:16 -0500

    add(sdk)

--- src/actions/Profil/profil.ts
+++ src/actions/Profil/profil.ts
@@ -1,9 +1,7 @@
-
-
-import axios from "axios";
 import {IStateProfilApp} from "../../reducers/Profil/Profil";
 import {Dispatch} from "redux";
 import User from "../../models/User";
+import { sdk } from "../../sdk/ugram";
 
 export enum ActionTypes {
     PROFIL = 'PROFIL',
@@ -18,9 +16,7 @@ export interface UserProfilAction { type: ActionTypes, payload: IStateProfilApp 
  * We are returning the right Action for each function
  */
 
-
 export function closeEdit(user : User) : any {
-    /**@todo api*/
     return function (dispatch: Dispatch<UserProfilAction>) {
         dispatch(  {
             type: ActionTypes.CLOSE_EDIT_PROFIL,
@@ -32,17 +28,12 @@ export function closeEdit(user : User) : any {
 }
 
 export function editUser(user: User) : any {
-    /**@todo api*/
     return function (dispatch: Dispatch<UserProfilAction>) {
-        axios.put('http://api.ugram.net/users/' + user.id, {
+        sdk.editUser(user.id, {
             email: user.email,
             firstName: user.firstName,
             lastName: user.lastName,
             phoneNumber: user.phoneNumber
-        }, {
-            headers: {
-                Authorization: 'Bearer ' + "91935b05-358b-4f41-aa79-8d6248d63637",
-            },
         }).then(function () {
             return dispatch(profilData(user.id));
         })
@@ -63,7 +54,7 @@ export function editUser(user: User) : any {
 
 export function profilData(userid): any {
     return function(dispatch : Dispatch<UserProfilAction>) {
-        return axios.get('http://api.ugram.net/users/' + userid)
+        return sdk.getUser(userid)
             .then(function (user) {
                 dispatch(  {
                     type: ActionTypes.PROFIL,
