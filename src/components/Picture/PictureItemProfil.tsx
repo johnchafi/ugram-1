cb6aa437fdb6219bee841194e158a6d9f5198a5c
Sébastien Bruere <sebastien.bruere@epitech.eu>
2019-02-17 12:22:18 -0500

    Fix horizontal scroll on mobile

--- src/components/Picture/PictureItemProfil.tsx
+++ src/components/Picture/PictureItemProfil.tsx
@@ -44,7 +44,7 @@ class PictureItemProfil extends React.Component<Props,State> {
 
     render() {
         return (
-            <Grid item xs={4}>
+            <Grid className="containerPictureProfil" item xs={4}>
                 <Grid  onClick={this.handleOpenEdit} className="pictureProfil" style={{backgroundImage: "url(" + this.props.picture.url + ")"}}/>
                 <Dialog onClose={this.handleCloseEdit} scroll="body" open={this.state.open}>
                     <PictureItem user={this.props.user} picture={this.props.picture} isHome={false}/>
