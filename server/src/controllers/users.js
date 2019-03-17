97e8131d95626f8049450a69690676be9860edaf
Henri Longle <henri.longle@epitech.eu>
2019-03-17 15:50:42 -0400

    fix previous merge

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -111,6 +111,7 @@ exports.deleteUser = (req, res, next) => {
 
 // Gets the pictures of a user
 exports.getUserPictures = (req, res, next) => {
+
     UserModel.findByPk(req.params.userId).then(user => {
         if (user === null) {
             return auth.sendError(res, "User '" + req.params.userId + "' does not exist.", 400)
@@ -138,67 +139,45 @@ exports.getUserPictures = (req, res, next) => {
                 }).then(tags => {
                     MentionModel.findAll({
                         where: {
-                            userId: req.params.userId
+                            pictureId: ids
                         },
                         order: [
-                            ['createdDate', 'DESC']
+                            ['id', 'ASC']
                         ]
-                    }).then(pictures => {
-                        let ids = [];
+                    }).then(mentions => {
                         pictures.forEach(picture => {
-                            ids.push(picture.id);
-                        });
-                        TagModel.findAll({
-                            where: {
-                                pictureId: ids
-                            },
-                            order: [
-                                ['id', 'ASC']
-                            ]
-                        }).then(tags => {
-                            MentionModel.findAll({
-                                where: {
-                                    pictureId: ids
-                                },
-                                order: [
-                                    ['id', 'ASC']
-                                ]
-                            }).then(mentions => {
-                                pictures.forEach(picture => {
-                                    let finalTags = [];
-                                    tags.forEach(tag => {
-                                        if (tag.pictureId == picture.id) {
-                                            finalTags.push(tag);
-                                        }
-                                    });
-                                    let finalMentions = [];
-                                    mentions.forEach(mention => {
-                                        if (mention.pictureId == picture.id) {
-                                            finalMentions.push(mention);
-                                        }
-                                    });
-                                    PictureModel.formatToClient(picture, finalMentions, finalTags);
-                                })
-                                return auth.sendSuccess(
-                                    res,
-                                    pagination.formatPagination(pictures, req.query.page, req.query.perPage),
-                                    200
-                                );
-                            }).catch(err => {
-                                return auth.sendError(res, err, 400);
+                            let finalTags = [];
+                            tags.forEach(tag => {
+                                if (tag.pictureId == picture.id) {
+                                    finalTags.push(tag);
+                                }
                             });
-                        }).catch(err => {
-                            return auth.sendError(res, err, 400);
-                        });
+                            let finalMentions = [];
+                            mentions.forEach(mention => {
+                                if (mention.pictureId == picture.id) {
+                                    finalMentions.push(mention);
+                                }
+                            });
+                            PictureModel.formatToClient(picture, finalMentions, finalTags);
+                        })
+                        return auth.sendSuccess(
+                            res,
+                            pagination.formatPagination(pictures, req.query.page, req.query.perPage),
+                            200
+                        );
                     }).catch(err => {
                         return auth.sendError(res, err, 400);
-                    })
-                })
-            })
+                    });
+                }).catch(err => {
+                    return auth.sendError(res, err, 400);
+                });
+            }).catch(err => {
+                return auth.sendError(res, err, 400);
+            });
         }
     }).catch(err => {
         return auth.sendError(res, "User '" + req.params.userId + "' does not exist.", 400)
-    })
+    });
 };
 
 // Uploads a picture for a user
@@ -208,56 +187,55 @@ exports.addUserPicture = (req, res, next) => {
         var form = new multiparty.Form();
 
         form.parse(req, function(err, fields, files) {
-            if (files.file) {
+            if (files && files.file) {
                 const extension = path.extname(files.file[0].originalFilename);
                 PictureModel.create({
                     description : fields.description[0],
                     extension : extension,
                     userId : user.id,
-                })
-                    .then(picture => {
-                        let tags = [];
-                        fields.tags.forEach(tag => {
-                            const nestedTags = tag.split(",");
-                            nestedTags.forEach(nestedTag => {
-                                tags.push({ value: nestedTag, pictureId: picture.id });
-                            });
-                        })
-                        TagModel.bulkCreate(tags).then(tags => {
-                            let mentions = [];
-                            fields.mentions.forEach(mention => {
-                                const nestedMentions = mention.split(",");
-                                nestedMentions.forEach(nestedMention => {
-                                    mentions.push({ userId: nestedMention, pictureId: picture.id });
-                                });
-                            })
-                            MentionModel.bulkCreate(mentions).then(mentions => {
-                                files.file[0].originalFilename = picture.id + extension;
-                                const errCallback = (err) => {
-                                    return auth.sendError(res, 'Cannot upload file', 500);
-                                }
-                                const succCallback = (url) => {
-                                    return auth.sendSuccess(res, {id: picture.id}, 200);
-                                }
-                                service.addUserPicture(req.params.userId, fields, files.file[0], errCallback, succCallback);
-                            }).catch(err => {
-                                return auth.sendError(res, 'Cannot insert mentions', 500);
-                            })
-                        }).catch(err => {
-                            return auth.sendError(res, 'Cannot insert tags', 500);
+                }).then(picture => {
+                    let tags = [];
+                    fields.tags.forEach(tag => {
+                        const nestedTags = tag.split(",");
+                        nestedTags.forEach(nestedTag => {
+                            tags.push({ value: nestedTag, pictureId: picture.id });
                         });
                     })
+                    TagModel.bulkCreate(tags).then(tags => {
+                        let mentions = [];
+                        fields.mentions.forEach(mention => {
+                            const nestedMentions = mention.split(",");
+                            nestedMentions.forEach(nestedMention => {
+                                mentions.push({ userId: nestedMention, pictureId: picture.id });
+                            });
+                        })
+                        MentionModel.bulkCreate(mentions).then(mentions => {
+                            files.file[0].originalFilename = picture.id + extension;
+                            const errCallback = (err) => {
+                                return auth.sendError(res, 'Cannot upload file', 500);
+                            }
+                            const succCallback = (url) => {
+                                return auth.sendSuccess(res, {id: picture.id}, 200);
+                            }
+                            service.addUserPicture(req.params.userId, fields, files.file[0], errCallback, succCallback);
+                        }).catch(err => {
+                            return auth.sendError(res, 'Cannot insert mentions', 500);
+                        })
+                    }).catch(err => {
+                        return auth.sendError(res, 'Cannot insert tags', 500);
+                    });
+                })
                     .catch(err => {
+                        console.log(err);
                         return auth.sendError(res, err, 500);
                     });
             } else {
                 return auth.sendError(res, 'No file provided', 400);
             }
         });
-    })
-        .catch(err => {
-            return auth.sendError(res, err.message, err.code);
-        });
+    })  .catch(err => {
+        return auth.sendError(res, err.message, err.code);
+    });
 };
 
 // Gets a single picture
@@ -375,6 +353,6 @@ exports.deleteUserPicture = (req, res, next) => {
         })
     }).catch(err => {
         console.log(err);
-        return auth.sendError(res, err, 401);
+        return auth.sendError(res, err.message, err.code);
     });
 };
