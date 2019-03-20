98a32807dda013165f325875c978010d94b17168
Arthur <arthur.klein@epitech.eu>
2019-03-19 21:39:59 -0400

    adding env

--- server/index.js
+++ server/index.js
@@ -5,13 +5,18 @@ const cors = require('cors');
 const path = require('path');
 const errors = require('./src/common/errors');
 const logger = require('./src/common/logger');
+const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: 'https://535ecc5a93654d4fab876372a40565e4@sentry.io/1419323' });
+
+// The request handler must be the first middleware on the app
 
 const routes = require('./src/routes/routes');
 
 const port = process.env.PORT || 80;
 
 const app = express();
-
+app.use(Sentry.Handlers.requestHandler());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
