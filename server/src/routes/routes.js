0d4243f1bf8de3a3b8d76e242470585e294a6c56
Henri Longle <henri.longle@epitech.eu>
2019-03-24 02:40:05 -0400

    swagger doc

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,35 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-const swaggerUiAssetPath = require('swagger-ui-dist').getAbsoluteFSPath();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../documentation/swagger-doc-ugram.json');
 
 const root = require('../controllers/root');
 const pictures = require('./pictures');
 const users = require('./users');
 const login = require('./login');
 
+
 router.use('/pictures', pictures);
 router.use('/users', users);
 router.use('/login', login);
 
-/**
- * @swagger
- * /info:
- *   get:
- *     tags:
- *       - api documentation json
- *     description: Get api documentation route information
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: All Api Doc informations
- */
 router.get('/info', root.getInfo);
 
 // Tricky part to change default swagger url
-router.get('/', root.get);
+//router.get('/', root.get);
 router.get('/index.html', root.get);
-router.use('/', express.static(swaggerUiAssetPath))
+//router.use('/', express.static(swaggerUiAssetPath));
+router.use('/', swaggerUi.serve);
+router.get('/', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
