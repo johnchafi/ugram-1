53c407a00a8aeb65c2f285e09a4b15e636ba3fa9
longle_h <longle_h@DESKTOP-OEH668A>
2019-04-21 02:18:41 -0400

    clean the unit test

--- server/test/user.js
+++ server/test/user.js
@@ -1,8 +1,6 @@
 //During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 
-let User = require('../src/models/user');
-
 //Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
@@ -10,9 +8,6 @@ let server = require('../index');
 let token = "";
 let userId = "unitTestUserThatShouldBeCreatedUT";
 let should = chai.should();
-let pictureId = 0;
-let likeId = 0;
-let commentId = 0;
 
 chai.use(chaiHttp);
 //Our parent block
@@ -25,30 +20,16 @@ let user = {
     phoneNumber: "0"
 };
 
-let userUdpate = {
+let userUpdate = {
     firstName: "string",
     lastName: "string",
     email: "utututu@uniTestUserUniqueForTesting.com",
     phoneNumber: "01234"
 };
 
-let like = {
-    userId : userId,
-    pictureId : pictureId,
-    ownerId: userId
-};
+describe('User manipulation', () => {
 
-let comment = {
-    userId: userId,
-    pictureId: pictureId,
-    message: "Hello world",
-    ownerId: userId
-};
-
-
-describe('Users unit test', () => {
-
-    describe('Create a user /POST/users', () => {
+    describe('/POST/users', () => {
         it('it should create a new user', (done) => {
             chai.request(server.app)
                 .post('/users')
@@ -61,7 +42,7 @@ describe('Users unit test', () => {
         });
     });
 
-    describe('Create a duplicate user /POST/users', () => {
+    describe('Error check /POST/users', () => {
         it('it should return already existing user', (done) => {
             chai.request(server.app)
                 .post('/users')
@@ -74,7 +55,7 @@ describe('Users unit test', () => {
     });
 
 
-    describe('Get a user /GET/users', () => {
+    describe('/GET/users', () => {
         it('it should get the new user', (done) => {
             chai.request(server.app)
                 .get('/users/' + userId)
@@ -90,12 +71,12 @@ describe('Users unit test', () => {
         });
     });
 
-    describe('Update a user /PUT/users', () => {
+    describe('/PUT/users', () => {
         it('it should get the new user', (done) => {
             chai.request(server.app)
                 .put('/users/' + userId)
                 .set('Authorization', 'Bearer ' + token)
-                .send(userUdpate)
+                .send(userUpdate)
                 .end((err, res) => {
                     res.should.have.status(201);
                     done();
@@ -111,151 +92,15 @@ describe('Users unit test', () => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
                     res.body.should.have.property('id').eql(user.id);
-                    res.body.should.have.property('email').eql(userUdpate.email);
-                    res.body.should.have.property('firstName').eql(userUdpate.firstName);
-                    res.body.should.have.property('lastName').eql(userUdpate.lastName);
+                    res.body.should.have.property('email').eql(userUpdate.email);
+                    res.body.should.have.property('firstName').eql(userUpdate.firstName);
+                    res.body.should.have.property('lastName').eql(userUpdate.lastName);
                     done();
                 });
         });
     });
 
-    describe('Get user pictures /GET/{userId}/pictures', () => {
-        it('it should get the user pictures', (done) => {
-                chai.request(server.app)
-                .get('/users/' + userId + '/pictures')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    done();
-                });
-        });
-    });
-
-    describe('Upload a picture for the user /POST/{userId}/pictures', () => {
-        it('it should upload a picture for the user', (done) => {
-            chai.request(server.app)
-                .post('/users/' + user.id + '/pictures')
-                .set('Authorization', 'Bearer ' + token)
-                .field('description', 'Test')
-                .field('mentions', userId)
-                .field('tags', 'test')
-                .attach('file', './test/test.jpg').end((err, res) => {
-                res.should.have.status(200);
-                pictureId = res.body.id;
-                comment.pictureId = pictureId;
-                done();
-            })
-        })
-    });
-
-    describe('Get a fake uploaded picture /GET/{userId]/pictures/{pictureId]', () => {
-        it('it should get 404', (done) => {
-            chai.request(server.app)
-                .get('/users/' + user.id + '/pictures/' + -1)
-                .end((err, res) => {
-                    res.should.have.status(404);
-                    done();
-                })
-        })
-    });
-
-    describe('Get a freeshly uploaded picture /GET/{userId]/pictures/{pictureId]', () => {
-        it('it get the new picture uploaded', (done) => {
-            chai.request(server.app)
-                .get('/users/' + user.id + '/pictures/' + pictureId)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    done();
-                })
-        })
-    });
-
-    // Like picture
-    describe('Like a picture /POST/users/{userId}/pictures/{pictureId}/like', () => {
-        it('it should like the picture', (done) => {
-            chai.request(server.app)
-                .post('/users/' + userId + '/pictures/' + pictureId + '/like')
-                .set('Authorization', 'Bearer ' + token)
-                .send(like)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    likeId = res.body.comment.id;
-                    done();
-                });
-        });
-    });
-
-    // Check notifications
-    describe('Get Notification /GET/users/{userId}/notifications', () => {
-        it('it should get the notifications', (done) => {
-            chai.request(server.app)
-                .get('/users/' + userId + '/notifications')
-                .set('Authorization', 'Bearer ' + token)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    done();
-                });
-        });
-    });
-
-    // Unlike picture
-
-    describe('Deleting the like /DELETE/users/{userId}/pictures/{pictureId}/like/{likeId}', () => {
-        it('it should delete the like', (done) => {
-            chai.request(server.app)
-                .delete('/users/' + userId  + '/pictures/' + pictureId + '/like/' + likeId)
-                .set('Authorization', 'Bearer ' + token)
-                .end((err, res) => {
-                    console.log(res);
-                    res.should.have.status(204);
-                    done();
-                });
-        });
-    });
-
-    // Comment picture
-    describe('Commenting the picture /POST/{userId}/pictures/{pictureId}/comment', () => {
-        it('it should comment the picture', (done) => {
-            chai.request(server.app)
-                .post('/users/' + userId  + '/pictures/' + pictureId + '/comment')
-                .set('Authorization', 'Bearer ' + token)
-                .send(comment)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    commentId = res.body.comment.id;
-                    done();
-                });
-        });
-    });
-
-
-    // Delete a comment
-    describe('Deleting the comment /DELETE/{userId}/pictures/{pictureId}/comment/{commentId]', () => {
-        it('it should delete the comment', (done) => {
-            chai.request(server.app)
-                .delete('/users/' + userId  + '/pictures/' + pictureId + '/comment/' + commentId)
-                .set('Authorization', 'Bearer ' + token)
-                .end((err, res) => {
-                    res.should.have.status(204);
-                    done();
-                });
-        });
-    });
-
-    // Delete picture
-    describe('Deleting the picture /DELETE/{userId}/pictures/{pictureId}', () => {
-        it('it should delete the picture', (done) => {
-            chai.request(server.app)
-                .delete('/users/' + userId  + '/pictures/' + pictureId)
-                .set('Authorization', 'Bearer ' + token)
-                .end((err, res) => {
-                    res.should.have.status(204);
-                    done();
-                });
-        });
-    });
-
-
-    describe('Get a fake user /GET/users', () => {
+    describe('Error check /GET/users', () => {
         it('it should return 404', (done) => {
             chai.request(server.app)
                 .get('/users/TESTBEAUCOUPTROPIMPROBABLE')
@@ -266,8 +111,8 @@ describe('Users unit test', () => {
         });
     });
 
-    describe('Deleting the user /DELETE/users', () => {
-        it('it should delete the new user', (done) => {
+    describe('/DELETE/users', () => {
+        it('it should delete the user', (done) => {
             chai.request(server.app)
                 .delete('/users/' + userId)
                 .set('Authorization', 'Bearer ' + token)
